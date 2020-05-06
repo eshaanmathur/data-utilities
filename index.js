5e0df1207bec7a738e2986bdcc19b2d4d71c4143
Eshaan Mathur <eshaan.mathur@gmail.com>
2020-05-06 08:19:48 +0530

    set a index file in the lib folder

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const unique = require('./lib/unique');
-const count = require('./lib/count');
-const rank = require('./lib/rank');
-const parseCsv = require('./lib/parseCsv');
-const parseSheet = require('./lib/parseSheet');
-const reportToTable = require('./lib/reportToTable');
-const tableToSheet = require('./lib/tableToSheet');
-const percentage = require('./lib/percentage');
-const tableToReport = require('./lib/tableToReport');
-
-module.exports = { unique, count, rank, parseCsv, parseSheet, reportToTable, tableToSheet, percentage, tableToReport };
+module.exports = require('./lib');
