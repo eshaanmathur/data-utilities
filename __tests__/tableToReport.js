5e3433adea3dfda387b940201a1f3d442468e4a4
Eshaan Mathur <eshaan.mathur@gmail.com>
2020-05-14 16:30:19 +0530

    report now takes unique keys and an array of values

--- __tests__/tableToReport.js
+++ __tests__/tableToReport.js
@@ -12,11 +12,22 @@ describe('tableToReport', () => {
       age: 28,
       email: 'jane@email.com',
     },
+    {
+      name: 'Json',
+      age: 30,
+      email: 'test@email.com',
+    },
   ];
 
   const output = new Map([
-    [30, { name: 'Jon Doe', email: 'jon@email.com' }],
-    [28, { name: 'Jane Doe', email: 'jane@email.com' }],
+    [
+      30,
+      [
+        { name: 'Jon Doe', email: 'jon@email.com' },
+        { name: 'Json', email: 'test@email.com' },
+      ],
+    ],
+    [28, [{ name: 'Jane Doe', email: 'jane@email.com' }]],
   ]);
 
   const result = tableToReport(input, 'age', ['name', 'email']);
