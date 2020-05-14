10771706927343f86ef0a1dc9ed34f642d7254e1
Eshaan Mathur <eshaan.mathur@gmail.com>
2020-05-14 13:54:22 +0530

    you can now pick the keys

--- __tests__/tableToReport.js
+++ __tests__/tableToReport.js
@@ -15,11 +15,11 @@ describe('tableToReport', () => {
   ];
 
   const output = new Map([
-    [30, { name: 'Jon Doe', age: 30, email: 'jon@email.com' }],
-    [28, { name: 'Jane Doe', age: 28, email: 'jane@email.com' }],
+    [30, { name: 'Jon Doe', email: 'jon@email.com' }],
+    [28, { name: 'Jane Doe', email: 'jane@email.com' }],
   ]);
 
-  const result = tableToReport(input, 'age');
+  const result = tableToReport(input, 'age', ['name', 'email']);
 
   test('should match the output', () => {
     expect(result).toEqual(output);
